5f3a4e36fe625aa8e02d5e84c6183ad877072adf
Alexey Porfirev <porfirev.alexey@gmail.com>
2021-12-29 16:52:29 +0400

    homework added

--- src/main/java/ru/netology/JvmExperience.java
+++ src/main/java/ru/netology/JvmExperience.java
@@ -11,22 +11,22 @@ import java.util.Set;
 public class JvmExperience {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Please open 'ru.netology.JvmExperience' in VisualVm");
-        Thread.sleep(30_000);
+        Thread.sleep(5_000);
 
         loadToMetaspaceAllFrom("io.vertx");
-        Thread.sleep(3_000);
+        Thread.sleep(5_000);
         loadToMetaspaceAllFrom("io.netty");
-        Thread.sleep(3_000);
+        Thread.sleep(5_000);
         loadToMetaspaceAllFrom("org.springframework");
-        Thread.sleep(3_000);
+        Thread.sleep(5_000);
 
         System.out.println(LocalTime.now() + ": now see heap");
         List<SimpleObject> simpleObjects = createSimpleObjects(5_000_000);
-        Thread.sleep(3_000);
+        Thread.sleep(5_000);
         simpleObjects.addAll(createSimpleObjects(5_000_000));
-        Thread.sleep(3_000);
+        Thread.sleep(5_000);
         simpleObjects.addAll(createSimpleObjects(5_000_000));
-        Thread.sleep(3_000);
+        Thread.sleep(5_000);
     }
 
     static void loadToMetaspaceAllFrom(String packageName) {
